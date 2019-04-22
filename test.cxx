9981790f551b9d8dfceab7c76ae73e0b3277cc90
ShapeOfMatter <mako@pop-os.laptop>
2019-04-22 15:05:58 -0400

    update teat file

--- test.cxx
+++ test.cxx
@@ -1,21 +1,8 @@
-#include <iostream>
-#include <stdexcept>
+#include "blsig_includes.h"
 
-#include "cryptopp810/rsa.h"
-#include "cryptopp810/sha.h"
-#include "cryptopp810/osrng.h"
-#include "cryptopp810/integer.h"
-#include "cryptopp810/cryptlib.h"
-#include "cryptopp810/nbtheory.h"
-
-using std::cout;
-using std::endl;
-using std::string;
-using std::runtime_error;
 using namespace CryptoPP;
 
-#define DEBUG    1
-#define KEY_SIZE 2048
+#define DEBUG 1
 
 static AutoSeededRandomPool rng_source;
 
@@ -46,29 +33,22 @@ int main(int argc, char *argv[])
     GenerateTestKeys(private_key, public_key, KEY_SIZE);
 
     // Alice create a blind message
-    Integer client_secret = GenerateClientSecret(public_key);
+    Integer client_secret = GenerateClientSecret(public_key, rng_source);
     string message = "Hello world! How are you doing to day? It's a pretty nice day if i do say so myself1.";
     Integer original_hash = GenerateHash(message);
     Integer blinded = MessageBlinding(original_hash, public_key, client_secret);
 
     // Send blinded message for signing
-    Integer signed_blinded = SigningAuthority(private_key, blinded);
-
-    // Alice will verify signature
-    if (!VerifySigning(signed_blinded, blinded, public_key))
-    {
-        cout << "Alice verification failed" << endl;
-        exit(EXIT_FAILURE);
-    }
+    Integer signed_blinded = SignBlindedMessage(private_key, blinded, rng_source);
 
     // Alice will remove blinding factor
-    Integer signed_unblinded = MessageUnblinding(signed_blinded, client_secret, public_key);
+    Integer signed_unblinded = SignatureUnblinding(signed_blinded, public_key, client_secret);
 
     // Eve verification stage
     Integer message_hash = GenerateHash(message);
     Integer received_hash = public_key.ApplyFunction(signed_unblinded);
     cout << "Signature payload: " << received_hash << endl;
-    if (message_hash != received_hash)
+    if (!VerifySignature(signed_unblinded, message_hash, public_key))
     {
         cout << "Verification failed" << endl;
         exit(EXIT_FAILURE);
@@ -76,6 +56,6 @@ int main(int argc, char *argv[])
 
     cout << "Signature Verified" << endl;
     // return success
-    return 0;
+    return EXIT_SUCCESS;
 }
 
